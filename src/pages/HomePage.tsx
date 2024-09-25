17aad0cc52a856e5d3d4fe7bb9576ccac1d466c9
Matt Wang <matthework@hotmail.com>
2024-09-25 15:48:22 +1200

    updated UI

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,10 @@ function HomePage() {
       <h1>Welcome to home page!</h1>
       <p>
         This template provides a minimal setup to kickstart a React app with
-        TypeScript + Material UI, which use Webpack + Babel as build tool with
-        configs.
+        TypeScript + Material UI, by using Webpack + Babel as build tools with
+        configs (dev + prod).
       </p>
+      <p className="App-footer">Developed by Matt Wang @ 2024</p>
     </>
   );
 }
